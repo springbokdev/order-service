be257c8534e66e705cadcd5335330051bf71ac39
John Spangenberg <dev@springbok.space>
2019-06-14 17:55:03 +0200

    Experiment with Zuul Filters.

--- src/main/java/space/springbok/orderservice/OrderServiceApplication.java
+++ src/main/java/space/springbok/orderservice/OrderServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +34,10 @@ public class OrderServiceApplication {
         return "Hello from instance " + instance;
     }
 
+    @RequestMapping("/fromLocation")
+    public String fromLocation(@RequestHeader("x-location") String location) {
+        return "Hello from location: " + location;
+    }
     @RequestMapping("/printConfig")
     public String printConfig() {
         StringBuilder sb = new StringBuilder();
